53a549451a4ad0ed79cdfbc87850a924eacc0041
Shawn Renner <shawn.renner@gmail.com>
2023-03-26 17:28:18 -0500

    baseline neopixel test

--- src/code.py
+++ src/code.py
@@ -1,1 +1,26 @@
-print("Hello World!")
+import time
+import board
+import neopixel
+import digitalio
+import analogio
+import colors
+
+## SETUP #############################################
+
+led = neopixel.NeoPixel(board.NEOPIXEL, 1)
+
+## LOOP ##############################################
+
+while True:
+    led.fill(colors.led_red)
+    time.sleep(1)
+    led.fill(colors.led_green)
+    time.sleep(1)
+    led.fill(colors.led_blue)
+    time.sleep(1)
+    led.fill(colors.led_yellow)
+    time.sleep(1)
+    led.fill(colors.led_white)
+    time.sleep(1)
+    led.fill(colors.led_off)
+    time.sleep(1)
---
+++ src/colors.py
@@ -0,0 +1,6 @@
+led_white = (255, 255, 255)
+led_red = (255, 0, 0)
+led_green = (0, 255, 0)
+led_blue = (0, 0, 255)
+led_yellow = (255, 255, 0)
+led_off = (0, 0, 0)
